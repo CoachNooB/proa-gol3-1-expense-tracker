6fae8df10a6c86dffac4586a80478410ef740f70
Antonius Harry <harry.antonius.pro@gmail.com>
2022-08-07 12:03:16 +0700

    add task service and API

--- backend/main.go
+++ backend/main.go
@@ -11,9 +11,17 @@ func main() {
 	app := gin.Default()
 	app.Use(cors.Default())
 
+	// Expenses Route
 	app.GET("/api/v1/expenses", services.GetExpenses)
 	app.POST("/api/v1/expenses", services.CreateRecord)
 	app.PUT("/api/v1/expenses/:id", services.UpdateRecordById)
 	app.DELETE("/api/v1/expenses/:id", services.DeleteRecordById)
+
+	// Tasks Route
+	app.GET("/api/v1/tasks", services.GetTasks)
+	app.POST("/api/v1/tasks", services.CreateTask)
+	app.PUT("/api/v1/tasks/:id", services.UpdateTaskById)
+	app.DELETE("/api/v1/tasks/:id", services.DeleteTaskById)
+
 	app.Run("localhost:5000")
 }
---
+++ backend/services/tasks.go
@@ -0,0 +1,77 @@
+package services
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type Task struct {
+	ID       string `json:"id,omitempty"`
+	Task     string `json:"type" binding:"required"`
+	Assignee string `json:"amount" binding:"required"`
+	Done     bool   `json:"description" binding:"required"`
+	Date     string `json:"date" binding:"required"`
+}
+
+var tasks []Task
+
+func GetTasks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "Success", "tasks": tasks})
+	return
+}
+
+func CreateTask(c *gin.Context) {
+	var newTask Task
+	if err := c.ShouldBindJSON(&newTask); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	newTask.ID = uuid.New().String()
+	tasks = append(tasks, newTask)
+	c.IndentedJSON(http.StatusCreated, gin.H{"message": "Task Added", "tasks": tasks})
+	return
+}
+
+func DeleteTaskById(c *gin.Context) {
+	id := c.Param("id")
+	index, found := taskById(id)
+	if !found {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Cannot Find Task"})
+		return
+	}
+	tasks = append(tasks[:index], tasks[index+1:]...)
+	return
+}
+
+func UpdateTaskById(c *gin.Context) {
+	id := c.Param("id")
+	index, found := taskById(id)
+	if !found {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Cannot Find Task"})
+		return
+	}
+	var newTask Task
+	if err := c.Bind(&newTask); err != nil {
+		return
+	}
+	currentData := tasks[index]
+
+	// Delete Current Task
+	tasks = append(tasks[:index], tasks[index+1:]...)
+	newTask.ID = currentData.ID
+	tasks = append(tasks, newTask)
+	c.IndentedJSON(http.StatusCreated, gin.H{"message": "Task Updated"})
+	return
+
+}
+
+func taskById(id string) (int, bool) {
+	for i, rec := range tasks {
+		if rec.ID == id {
+			return i, true
+		}
+	}
+	return 0, false
+}
